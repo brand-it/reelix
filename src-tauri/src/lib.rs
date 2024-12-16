47c0692c8d9089cbb35d0dcff226a65f28a86bb7
Brandt Lareau <brandt@lareaus.com>
2024-12-15 18:22:21 -0800

    Compile the html files into the binary

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,42 +1,31 @@
 mod commands;
 mod state;
 
+use include_dir::{include_dir, Dir};
 use state::AppState;
 use std::sync::Arc;
-use tauri::path::BaseDirectory;
-use tauri::Manager;
 use tera::Tera;
+
+static TEMPLATES_DIR: Dir = include_dir!("templates");
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    // let tera = Tera::new("resources/templates/**/*.html").unwrap_or_else(|e| {
-    //     eprintln!("Parsing error(s): {e}");
-    //     ::std::process::exit(1);
-    // });
 
-    // let app_state = AppState {
-    //     tera: Arc::new(tera),
-    // };
+    let mut tera = Tera::default();
+    // Takes all the template files and compiles them with the binary
+    for file in TEMPLATES_DIR.files() {
+        if let Some(path) = file.path().to_str() {
+            let content = file.contents_utf8().unwrap();
+            tera.add_raw_template(path, content)
+                .expect("Failed to add template");
+        }
+    }
+
+    let app_state = AppState {
+        tera: Arc::new(tera),
+    };
     tauri::Builder::default()
-        .setup(|app| {
-            // The path specified must follow the same syntax as defined in
-            // `tauri.conf.json > bundle > resources`
-            let template_path = app.path().resolve("templates", BaseDirectory::Resource)?;
-            // Convert the path to a string
-            let template_path_str = template_path
-                .to_str()
-                .ok_or("Failed to convert template path to string")?;
-            let tera = Tera::new(&format!("{template_path_str}/**/*.html")).unwrap_or_else(|e| {
-                eprintln!("Parsing error(s): {e}");
-                ::std::process::exit(1);
-            });
-
-            let app_state = AppState {
-                tera: Arc::new(tera),
-            };
-            app.manage(app_state);
-            Ok(())
-        })
+        .manage(app_state)
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![commands::greet, commands::about])
         .run(tauri::generate_context!())
